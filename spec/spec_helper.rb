5a53c8f9e3e933fd43945595031468fcaa08552d
Steven Li <sli@bleacherreport.com>
2014-10-04 03:39:43 -0700

    put code climate coverage in if block

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,8 @@ $LOAD_PATH.unshift File.expand_path("../lib", __FILE__)
 require "pry"
 require "extended_ruby"
 
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
+if ENV['CODECLIMATE_REPO_TOKEN']
+    require 'codeclimate-test-reporter'
+    CodeClimate::TestReporter.start
+end
 
