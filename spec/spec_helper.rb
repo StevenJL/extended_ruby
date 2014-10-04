fd0fe3177b0e9e18909ae693f71a46c7c8804cc6
Steven Li <sli@bleacherreport.com>
2014-10-04 03:44:42 -0700

    change spec_helper to pass build

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,9 +2,8 @@ $LOAD_PATH.unshift File.expand_path("../lib", __FILE__)
 
 require "pry"
 require "extended_ruby"
+require 'codeclimate-test-reporter'
 
-if ENV['CODECLIMATE_REPO_TOKEN']
-    require 'codeclimate-test-reporter'
-    CodeClimate::TestReporter.start
-end
+ENV['CODECLIMATE_REPO_TOKEN'] = "28887ba3bb316efd91ce239437120a3e02b49cea9cdca094500ea8ba7995084b"
+CodeClimate::TestReporter.start
 
